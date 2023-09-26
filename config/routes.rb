093986a49c35963893af1041718cac94ce3f0828
Shawn Thomas <shawnt41@gmail.com>
2023-09-26 12:57:14 +0200

    created restuarant controller + respective view pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-    root to: "pages#home" do
-      resources :restaurant
+    root to: "pages#home"
+
+    resources :users do
+      resources :restaurant, only: [:index, :new, :create, :show, :edit, :update]
+    end
 end

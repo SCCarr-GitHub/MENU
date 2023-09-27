333853d2999d38949297ff7f79be66532841cab8
Hugo Lennep <hugovanlennep@gmail.com>
2023-09-27 09:43:44 +0200

    Added profile photo funtionality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-
+  
   has_many :reviews, dependent: :destroy
   has_one :restaurant_menu, dependent: :destroy
   has_many :items, through: :restaurant_menu

ee03272b6f2702378a2ec3d13db9f34d35b7560a
Hugo Lennep <hugovanlennep@gmail.com>
2023-10-02 17:38:43 +0200

    Added ability for restaurant to upload and display their photos on their menu

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   include PgSearch::Model
   belongs_to :restaurant_menu
   has_many :ratings, dependent: :destroy
+  has_one_attached :photo
 
   multisearchable against:
       [ :item_name, :category ]
